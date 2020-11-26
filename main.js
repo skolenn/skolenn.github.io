00d18a1f646176c61711efdc0e23b22fb0b2dfe2
skolenn <73689278+skolenn@users.noreply.github.com>
2020-11-26 10:56:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+      var request = new XMLHttpRequest();
+      request.open("POST", "https://some-retarded-api.glitch.me");
 
+      request.setRequestHeader('Content-type', 'application/json');
+
+      var params = {
+        username: "My Webhook Name",
+        avatar_url: "",
+        content: "The message to send"
+      }
+
+      request.send(JSON.stringify(params));
