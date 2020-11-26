78080cb2e60d8e76729d321cc202db6bf3a43056
skolenn <73689278+skolenn@users.noreply.github.com>
2020-11-26 10:48:21 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-            let markdown = "`"
+let markdown = '`';
 
-            function popup(text) {
-            let p = document.createElement("div");
-            p.className = "popup";
-            let t = document.createElement("p");
-            t.innerHTML = text;
-            let a = document.createElement("a");
-            a.className = "popup-accept";
-            a.innerText = "Ok";
-            a.onclick = () => {
-                p.parentNode.removeChild(p);
-                location.reload()
-            };
+function popup(text) {
+	let p = document.createElement('div');
+	p.className = 'popup';
+	let t = document.createElement('p');
+	t.innerHTML = text;
+	let a = document.createElement('a');
+	a.className = 'popup-accept';
+	a.innerText = 'Ok';
+	a.onclick = () => {
+		p.parentNode.removeChild(p);
+		location.reload();
+	};
 
-            p.appendChild(t);
-            p.appendChild(a);
-            document.body.appendChild(p);
-        }
+	p.appendChild(t);
+	p.appendChild(a);
+	document.body.appendChild(p);
+}
 
-      function send() {
-      let request = new XMLHttpRequest();
-      request.open("POST", "https://peitho-messenger.glitch.me");
-      let text = document.getElementById("input").value
-      let name = document.getElementById("name").value
-      request.setRequestHeader('Content-type', 'application/json');
+function send() {
+	let request = new XMLHttpRequest();
+	request.open('POST', 'https://peitho-messenger.glitch.me');
+	let text = document.getElementById('input').value;
+	let name = document.getElementById('name').value;
+	request.setRequestHeader('Content-type', 'application/json');
 
-      let params = {
-        username: "skolenn.github.io",
-        avatar_url: "",
-        content: `${markdown}${text}${markdown} from ${markdown}${name}${markdown}`
-      }
-
-      request.send(JSON.stringify(params));
-      popup(`sent "${text}"`);
-      }
-
+	let params = {
+		username: 'skolenn.github.io',
+		avatar_url: '',
+		content: `${markdown}${text}${markdown} from ${markdown}${name}${markdown}`,
+	};
+	if (text == undefined) {
+		return popup("You can't send an empty string");
+	} else {
+		request.send(JSON.stringify(params));
+		popup(`sent "${text}"`);
+	}
+}
