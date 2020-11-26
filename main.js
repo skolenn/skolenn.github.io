da45afb245481d3e9206faf08769e4ac41007839
skolenn <73689278+skolenn@users.noreply.github.com>
2020-11-26 10:22:00 -0800

    Update main.js

--- main.js
+++ main.js
@@ -32,3 +32,20 @@
       alert(`sent ${text}`);
       location.reload()
       }
+
+        function popup(text) {
+            let p = document.createElement("div");
+            p.className = "popup";
+            let t = document.createElement("p");
+            t.innerHTML = text;
+            let a = document.createElement("a");
+            a.className = "popup-accept";
+            a.innerText = "Ok";
+            a.onclick = () => {
+                p.parentNode.removeChild(p);
+            };
+
+            p.appendChild(t);
+            p.appendChild(a);
+            document.body.appendChild(p);
+        }
